80c2f1aeb9a88d0c825b2661bc38e894b469cce1
Hrithik Neogi <hrneogi99@gmail.com>
2021-10-24 22:38:33 +0530

    added like button, implemented add and delete comment function

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, request, url_for
 from flask_login import login_required, current_user
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Like
 from . import db
 
 views = Blueprint("views", __name__)
@@ -66,16 +66,50 @@ def posts(username):
 
     return render_template("posts.html", user=current_user, posts=posts, username=username)
 
-@views.route("/create-comment/<post_id", methods=['POST'])
+@views.route("/create-comment/<post_id>", methods=['POST'])
 @login_required
 def create_comment(post_id):
     text = request.form.get('text')
 
-    if post:
+    if text:
         comment = Comment(text=text,author=current_user.id,post_id=post_id)
         db.session.add(comment)
         db.session.commit()
     else:
         flash('Please type something to add comment', category='error')
+
+    return redirect(url_for("views.home"))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exists', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('Sorry! You do not have permission to delete this', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for("views.home"))
+
+@views.route("/like-post/<post_id>", methods=['GET'])
+@login_required
+def like(post_id):
+    post = Comment.query.filter_by(id=post_id)
+    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        flash('Post does not exists', category='error')
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
 
     return redirect(url_for("views.home"))
